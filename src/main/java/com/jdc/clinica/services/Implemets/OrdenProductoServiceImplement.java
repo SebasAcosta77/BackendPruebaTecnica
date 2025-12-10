1da8994962aa2775fa90d365362c7007be304d86
SebasAcosta77 <jsacosta@jdc.edu.co>
2025-12-09 19:45:12 -0500

    Update OrdenProductoServiceImplement.java

--- src/main/java/com/jdc/clinica/services/Implemets/OrdenProductoServiceImplement.java
+++ src/main/java/com/jdc/clinica/services/Implemets/OrdenProductoServiceImplement.java
@@ -25,32 +25,32 @@ public class OrdenProductoServiceImplement implements IOrdenProductoService {
     @Autowired
     private IProductoRepository productoRepository;
 
-    // =========================================
+  
     // LISTAR TODOS
-    // =========================================
+   
     @Override
     public List<OrdenProductoEntity> findAll() {
         return ordenProductoRepository.findAll();
     }
 
-    // =========================================
+   
     // BUSCAR POR ID
-    // =========================================
+   
     @Override
     public OrdenProductoEntity findById(Long id) {
         return ordenProductoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("OrdenProducto no encontrado con ID: " + id));
     }
 
-    // =========================================
-    // CREAR / EDITAR
-    // =========================================
+   
+    // CREAR 
+   
     @Override
     public OrdenProductoEntity save(OrdenProductoDTO dto) {
 
         OrdenProductoEntity entity;
 
-        // EDITAR
+  
         if (dto.getIdordenproducto() != null) {
             entity = ordenProductoRepository.findById(dto.getIdordenproducto())
                     .orElseThrow(() ->
@@ -60,12 +60,12 @@ public class OrdenProductoServiceImplement implements IOrdenProductoService {
             entity = new OrdenProductoEntity();
         }
 
-        // ASOCIAR ORDEN
+       
         OrdenEntity orden = ordenRepository.findById(dto.getFkorden())
                 .orElseThrow(() -> new RuntimeException("No existe ORDEN con ID: " + dto.getFkorden()));
         entity.setOrden(orden);
 
-        // ASOCIAR PRODUCTO
+      
         ProductoEntity producto = productoRepository.findById(dto.getFkproducto())
                 .orElseThrow(() -> new RuntimeException("No existe PRODUCTO con ID: " + dto.getFkproducto()));
         entity.setProducto(producto);
@@ -76,9 +76,9 @@ public class OrdenProductoServiceImplement implements IOrdenProductoService {
         return ordenProductoRepository.save(entity);
     }
 
-    // =========================================
+   
     // ELIMINAR
-    // =========================================
+   
     @Override
     public void delete(Long id) {
         OrdenProductoEntity entity = ordenProductoRepository.findById(id)
